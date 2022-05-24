f05323a5e9838c494fcc8a65745982206f172a01
José Arvizu <joseantoniod63@gmail.com>
2022-05-24 18:31:04 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require('path');
+const mysql = require("mysql");
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const config = require("./config/dbconfig")
+
+dotenv.config({ path: './.env'});
+
+const app = express();
+
+const db = mysql.createConnection(config);
+
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: false }));
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+app.use(cookieParser());
+
+app.set('view engine', 'hbs');
+
+db.connect( (error) => {
+  if(error) {
+    console.log(error)
+  } else {
+    console.log("MYSQL Connected...")
+  }
+})
+
+//Define Routes
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
+
+app.listen(5001, () => {
+  console.log("Server started on Port 5001");
+})
